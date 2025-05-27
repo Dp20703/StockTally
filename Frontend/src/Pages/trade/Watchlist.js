22951dfd652ffe1253b7cffae8fc62c2e0d2e193
dp <codewithdp2073@gmail.com>
2025-05-27 23:36:45 +0530

    feat: enhance Watchlist and AllWatchlist components; implement fetching and displaying user watchlists with improved layout

--- Frontend/src/Pages/trade/Watchlist.js
+++ Frontend/src/Pages/trade/Watchlist.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import AllWatchlist from '../../components/watchlistCompo/AllWatchlist'
 
 const Watchlist = () => {
   return (
-    <div className='text-center'>
-      watchlist
+    <div className='min-vh-100 min-vw-100 bg-black'>
+      <div className="d-flex flex-column align-items-center justify-content-center w-100">
+        <div className='mx-5 mt-5'>
+          <button onClick={() => (true)} className="btn btn-primary">
+            + New Watchlist
+          </button>
+        </div>
+        <div className='mx-5 mt-5 w-100 '>
+          <AllWatchlist />
+        </div>
+      </div>
     </div>
   )
 }
--- Frontend/src/components/watchlistCompo/AllWatchlist.js
+++ Frontend/src/components/watchlistCompo/AllWatchlist.js
@@ -1,8 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Table from 'react-bootstrap/Table';
 
 const AllWatchlist = () => {
+  const [watchlists, setWatchlists] = useState([]);
+  useEffect(() => {
+    const fetchWatchlist = async () => {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        throw new Error("No token");
+      }
+      const res = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/watchlist/get`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      console.log("watchlist response:", res.data);
+      setWatchlists(res.data);
+    }
+    fetchWatchlist();
+  }, [])
   return (
-    <div className='text-center'>AllWatchlist</div>
+    <>
+      <div className='mb-5 text-center fs-1 text-bg-warning w-50 m-auto rounded-2'>
+        AllWatchlist
+      </div>
+
+      <div className='d-flex justify-content-center gap-3 flex-wrap w-100'>
+        {
+          watchlists.map((elem, idx) => {
+
+            return <div key={elem._id}>
+              <div className="text-bg-secondary rounded-2 overflow-hidden">
+
+                <h1 className='text-bg-success text-nowrap px-2 mb-0 py-1 fs-2'>
+                  <span className=' text-dark px-2'>{idx + 1}. </span>
+                  {elem.watchlistName}
+                  <span>
+                    <i className="ri-delete-bin-6-line fs-5 text-dark float-end mx-1 my-2" />
+                    <i className="ri-edit-box-line fs-5 text-dark float-end mx-1 my-2" />
+                  </span>
+                </h1>
+
+                <Table responsive className='table mb-0 table-hover table-dark  table-bordered'>
+                  <thead className='table-secondary'>
+                    <tr>
+                      <th>#</th>
+                      <th>Stock name</th>
+                      <th>Stock symbol</th>
+                      <th>Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                      elem.stocks.map((stock, idx) => {
+                        return <tr key={idx}>
+                          <td>{idx + 1}</td>
+                          <td>{stock.stockName}</td>
+                          <td>{stock.stockSymbol}</td>
+                          <td>{stock.price}</td>
+                        </tr>
+                      })
+                    }
+                  </tbody>
+                </Table>
+
+              </div>
+            </div>
+          })
+        }
+      </div>
+    </>
   )
 }
 
