f9ecb1ad8fca68ba779d433d47f749157c6f6750
dp <codewithdp2073@gmail.com>
2025-06-01 23:52:25 +0530

    feat: add profile picture update functionality; implement modal for file input and enhance user interaction

--- Frontend/src/Pages/users/Profile.js
+++ Frontend/src/Pages/users/Profile.js
@@ -1,20 +1,40 @@
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext'
 import NavbarCompo from '../../components/Navbar';
+import { useState } from 'react';
 
 const Profile = () => {
     const { user } = useAuth();
+    const [updateModal, setUpdateModal] = useState(false);
+
+    const handleUpdate = (e) => {
+        e.preventDefault();
+alert('submited')
+    }
     return (
         <div id='dashboard'>
-            <NavbarCompo/>
-            <div className='mt-5'>
-                <div style={{ background: '#13162F', color: 'white', backgroundPosition: 'center', backgroundSize: "cover", border: '.5px solid white' }} className="card m-auto bg-black text-center w-50 rounded-5 overflow-hidden">
+            <NavbarCompo />
+            <div className='mt-3'>
+                <div style={{ background: '#13162F', color: 'white', backgroundPosition: 'center', backgroundSize: "cover", border: '.5px solid white' }} className="card m-auto bg-black w-50 rounded-5 overflow-hidden">
                     <img
                         src={`https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 100)}.jpg`}
-                        className=" w-50 m-auto rounded-circle mt-3 "
-                        style={{ height: "25rem", objectFit: 'cover', objectPosition: 'center', border: '2px solid white', }}
+                        className=" m-auto rounded-circle mt-3 "
+                        style={{ height: "20rem", width: '20rem', objectFit: 'cover', objectPosition: 'center', border: '2px solid white', }}
                         alt="..." />
-                    <div className="card-body">
+
+                    <i className="ri-file-edit-fill position-absolute" style={{ top: '18rem', right: '15rem', fontSize: '1.5rem', cursor: 'pointer' }} onClick={() => { setUpdateModal(true); }} />
+
+                    <div className="card-body text-center">
+                        {
+                            updateModal &&
+                            <div className="py-3 w-50 m-auto border border-2 rounded-3">
+                                <input type="file" name="profilePic" />
+                                <div className="d-flex justify-content-between py-2 px-3">
+                                    <button className="btn btn-danger" onClick={() => { setUpdateModal(false); }}>Cancel</button>
+                                    <button type='submit' className="btn btn-success" onClick={handleUpdate}>Update</button>
+                                </div>
+                            </div>
+                        }
                         <p className="card-title">
                             <i className="ri-shield-user-fill fs-4" />
                             &nbsp; <span className='fs-4'>{user?.userName}</span>
@@ -32,7 +52,6 @@ const Profile = () => {
                             &nbsp;
                             <span className='fs-6'>
                                 {user?.email}</span></p>
-
                     </div>
                 </div>
             </div>
