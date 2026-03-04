0e07b29cfd3c6af2a08007bc8189db354d23f74b
dp <codewithdp2073@gmail.com>
2026-03-04 13:14:33 +0530

    refactor: remove Poster component and associated images; clean up routing in App

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,7 +12,6 @@ import Watchlist from './Pages/watchlist/Watchlist';
 import { StockCharts } from './Pages/charts/StockCharts';
 import TopStories from './Pages/charts/TopStories';
 import NotFound from './Pages/users/NotFound';
-import Poster from './Pages/Poster';
 
 const App = () => {
   return (
@@ -21,7 +20,6 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/poster' element={<Poster />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path="*" element={<NotFound />} />
